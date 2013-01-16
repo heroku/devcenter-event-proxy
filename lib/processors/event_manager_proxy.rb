a7b26cd4989a5eddb7fdf890f58628dccefe54f1
Ryan Daigle <ryan.daigle@gmail.com>
2013-01-16 13:13:43 -0500

    Make connection timeout opts config vars. Abstract config values

--- lib/processors/event_manager_proxy.rb
+++ lib/processors/event_manager_proxy.rb
@@ -3,11 +3,6 @@ require 'em-synchrony/em-http'
 
 class EventManagerProxy
 
-  EVENT_MANAGER_HEADERS = {
-    'content-type' => 'application/json',
-    'authorization' => ENV.values_at("EVENT_MANAGER_API_USER", "EVENT_MANAGER_API_PASSWORD")
-  }
-
   class << self
 
     def process(value_hashes)
@@ -15,12 +10,14 @@ class EventManagerProxy
       value_hashes.each do |value_hash|
         if(value_hash && value_hash.any?)
           body = JSON.dump(value_hash)
-          multi.add SecureRandom.uuid, EM::HttpRequest.new(ENV['EVENT_MANAGER_API_URL'], :connect_timeout => 3, :inactivity_timeout => 6).post(
-            body: body, head: EVENT_MANAGER_HEADERS
+          multi.add SecureRandom.uuid, EM::HttpRequest.new(connection_url, connection_opts).post(
+              body: body, head: connection_headers
           )
         end
       end if value_hashes
     end
+
+    #--- Request handling
 
     def build_multirequest
       multirequest = EventMachine::MultiRequest.new
@@ -42,6 +39,26 @@ class EventManagerProxy
 
     def handle_error(http)
       puts "at=error measure=event-drain.event-manager-proxy.event-errors status=#{http.response_header.status} err=\"#{http.error}\""
+    end
+
+    #--- Connection info
+
+    def connection_url
+      ENV['EVENT_MANAGER_API_URL']
+    end
+
+    def connection_headers
+      {
+        'content-type' => 'application/json',
+        'authorization' => ENV.values_at("EVENT_MANAGER_API_USER", "EVENT_MANAGER_API_PASSWORD")
+      }
+    end
+
+    def connection_opts
+      { 
+        :connect_timeout => ENV['EVENT_MANAGER_API_CONNECT_TIMEOUT_SECS'] || 9,
+        :inactivity_timeout => ENV['EVENT_MANAGER_API_INACTIVITY_TIMEOUT_SECS'] || 19
+      }
     end
   end
 end
